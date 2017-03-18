1891b4b9679f151e39ec70fbaab3325714753348
joeDiHare <stefano286@gmail.com>
2017-03-18 16:52:04 -0400

    switched to mysql

--- wotchat/settings.py
+++ wotchat/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'wotchat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wcdb',
+        'USER': 'joedihare',
+        'PASSWORD': 'lh2806',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
